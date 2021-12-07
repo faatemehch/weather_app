0c082afdfa0868516c0a4bcf5eda06ec85d4eb4a
faatemeh chamani <fch199674@yahoo.com>
2021-12-07 10:18:48 +0330

    view and templates added

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+'''
+    weather app in django using API.
+    https://openweathermap.org
+'''
+
+
+def index(request):
+    # the query(q) is the name of city
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d8f310c4694db72dd9df4accd477b9bd'
+    city = request.GET.get( 'q' )
+    if city is None:
+        city = 'Gorgan'
+    response = requests.get( url.format( city ) ).json()
+    # print( response )
+    # response now is a python dictionary
+    context = {
+        'city': response['name'],  # name of the city
+        'country': response['sys']['country'],  # name of the country of the city
+        'description': response['weather'][0]['description'],  # list of dictionary
+        'temperature': response['main']['temp'],
+        'icon': response['weather'][0]['icon']
+    }
+    return render( request, 'weather/weather-index.html', context )
