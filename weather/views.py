d931557878a38752a2ca4611b2d258d281857b3e
faatemeh chamani <fch199674@yahoo.com>
2021-12-08 10:45:53 +0330

    temperature converter

--- weather/views.py
+++ weather/views.py
@@ -10,10 +10,6 @@ import requests
 '''
 
 
-def convert_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * (5 / 9)
-
-
 def index(request):
     # the query(q) is the name of city
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d8f310c4694db72dd9df4accd477b9bd'
@@ -21,8 +17,6 @@ def index(request):
     if city_name is None:  # consider a city as default
         city_name = 'New York'
     response = requests.get( url.format( city_name ) ).json()  # response now is a python dictionary
-    temperature = 'F'
-    # print(response)
     try:
         context = {
             'city': response['name'],  # name of the city
@@ -31,10 +25,7 @@ def index(request):
             'temperature': response['main']['temp'],
             'icon': response['weather'][0]['icon']
         }
-        print( convert_to_celsius( context['temperature'] ) )
-
     except:
-        # print( response.get( 'code' ) )
         context = {
             'error': f'{city_name} City Not Found!'
         }
